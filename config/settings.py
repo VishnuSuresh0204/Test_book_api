dfbf33c78405f6583c7cb2d4faf7cadacf90756d
Vishnu Suresh <vishnu@example.com>
2026-07-02 14:42:57 +0530

    Update project configuration and add requirements

--- config/settings.py
+++ config/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-94w0!=blg0i8cqq8=)603^swyd3w+l%gnsgf)wi_x()xui(e@8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["test-book-api.onrender.com"]
 
 # Application definition
 
@@ -118,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
