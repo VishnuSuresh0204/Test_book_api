e3cef6ff21b76a061404c52c038731ef4eab355a
Vishnu Suresh <vishnu@example.com>
2026-07-02 14:18:20 +0530

    Initial Django student API project

---
+++ api/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+ 
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    published_year = models.IntegerField()
+ 
+    def __str__(self):
+        return self.title
